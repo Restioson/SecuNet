d6067b6d4b27db4d85ecfd0c38a610c96e690624
Restioson <restiosondev@gmail.com>
2017-04-14 19:12:46 +0200

    Fix updater directory

--- update.lua
+++ update.lua
@@ -2,5 +2,5 @@
 io.write("Installation directory: ")
 local dir = read()
 io.write("\n")
-shell.run("rm " .. dir .. "/apis/secunet/secunet.lua")
-shell.run("wget https://git.io/vSkXL " .. dir .. "/apis/secunet/secunet")
+shell.run("rm " .. dir .. "/apis/secunet.lua")
+shell.run("wget https://git.io/vSkXL " .. dir .. "/apis/secunet")
